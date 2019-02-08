c071b12be58b26d2648080bc10d50a8e33114807
Oscar Rydh <oscar.rydh.93@gmail.com>
2019-02-08 11:32:01 +0100

    Added database schema for the whiskies

---
+++ mysql_data/init/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS whiskies(
+  namn VARCHAR (100),
+  namn2 VARCHAR (100),
+  typ VARCHAR (100),
+  ursprunglandnamn VARCHAR (100),
+  ursprung VARCHAR (100),
+  alkoholhalt VARCHAR (100),
+  volymiml VARCHAR (100),
+  prisinklmoms VARCHAR (100),
+  artikelid VARCHAR (100)
+);
